0675393e06695672be255b590e419e2fb3cf9c4f
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-05-02 16:03:58 +0530

    done
    done

---
+++ src/components/ProtextedRoute.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navigate } from "react-router";
+
+export const ProtectedRoute = ({ children }) => {
+    if (!true) {
+        return <Navigate to="/login" replace={false} />;
+    }
+
+    return children;
+};
